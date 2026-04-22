95d08803d0c8480e22a49037566dea4fb66f6c39
nayarasantosalves9@gmail.com <nayarasantosalves9@gmail.com>
2026-04-22 11:49:24 -0300

    Estrutura da pasta assets, imagens aleatórias, email clicável e delete na tabela

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let dados = [];
 
-        const salvar = () => {
+        const salvar = (event) => {
+              event.preventDefault();
+
             const nome = document.getElementById('nome').value;
             const idade = document.getElementById('idade').value;
             const email = document.getElementById('email').value;
@@ -8,9 +10,19 @@ let dados = [];
             dados.push({nome, idade, email});
 
             const tabela = document.getElementById('Minha-tabela');
-            tabela.innerHTML +="<tr><td>" + nome + "</td><td>" + idade + "</td><td>" + email + "</td></tr>";
+           tabela.innerHTML += `<tr>
+            <td>${nome}</td>
+            <td>${idade}</td>
+            <td><a href="mailto:${email}">${email}</a></td>
+            <td><button onclick="deletar(this)">Deletar</button></td>
+            </tr>`;
 
             document.getElementById('nome').value = "";
             document.getElementById('idade').value = "";
             document.getElementById('email').value = "";
+        }
+
+            function deletar(botao) {
+            const linha = botao.closest("tr");
+            linha.remove();
         }
